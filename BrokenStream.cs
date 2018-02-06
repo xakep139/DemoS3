a24d848eb7829613a2db2ab870af9aff703e3350
Nikita Balabaev <n.balabaev@2Gis.ru>
2018-02-06 16:52:35 +0700

    first commit

---
+++ BrokenStream.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace DemoS3
+{
+    public class BrokenStream : Stream
+    {
+        public override void Flush()
+        {
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            throw new Exception("Oooops!");
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new NotSupportedException();
+        }
+
+        public override bool CanRead => true;
+        public override bool CanSeek => false;
+        public override bool CanWrite => false;
+        public override long Length => 123;
+        public override long Position { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+
+namespace DemoS3
+{
+    class Program
+    {
+        public const string BucketName = "vstore-objects-standalone";
+
+        static async Task Main(string[] args)
+        {
+            var credentials = new BasicAWSCredentials("accessKey", "secretKey");
+            var config = new AmazonS3Config
+            {
+                ServiceURL = "http://rgw.n3.hw"
+            };
+
+            using (var client = new AmazonS3Client(credentials, config))
+            {
+                using (var myBrokenStream = new BrokenStream())
+                {
+                    await WritingAnObject(client, myBrokenStream);
+                }
+            }
+        }
+
+        private static async Task WritingAnObject(IAmazonS3 client, Stream inputStream)
+        {
+            try
+            {
+                var request = new PutObjectRequest
+                {
+                    InputStream = inputStream,
+                    BucketName = BucketName,
+                    CannedACL = S3CannedACL.PublicRead,
+                    Key = "1/temp.txt"
+                };
+
+                await client.PutObjectAsync(request);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
